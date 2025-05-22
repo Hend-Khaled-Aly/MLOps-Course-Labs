2ba412f782eadf569fc0de9555c07560482fbcf0
Hend Khaled Aly <hendkhaledaly179@gmail.com>
2025-05-22 12:06:42 +0300

    Lab7

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import mlflow.pyfunc
 import joblib
+import time
+from prometheus_client import Counter, Histogram, generate_latest
+from fastapi.responses import Response
+
 
 
 app = FastAPI(title="Bank Customer Churn Prediction API")
@@ -10,6 +14,14 @@ app = FastAPI(title="Bank Customer Churn Prediction API")
 #model_stage = "Production"
 
 #model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_stage}")
+
+# Prometheus metrics
+REQUEST_COUNT = Counter(
+    "request_count", "Total number of requests", ["method", "endpoint"]
+)
+REQUEST_LATENCY = Histogram(
+    "request_latency_seconds", "Request latency in seconds", ["endpoint"]
+)
 
 # Define the input schema
 model = joblib.load("model.pkl")
@@ -26,6 +38,17 @@ class CustomerData(BaseModel):
     IsActiveMember: int
     EstimatedSalary: float
 
+@app.middleware("http")
+async def prometheus_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+
+    REQUEST_COUNT.labels(method=request.method, endpoint=request.url.path).inc()
+    REQUEST_LATENCY.labels(endpoint=request.url.path).observe(process_time)
+
+    return response
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Churn Prediction API"}
@@ -40,3 +63,8 @@ def predict(data: CustomerData):
     input_df = [input_df]  
     prediction = model.predict(input_df)
     return {"prediction": int(prediction[0])}
+
+# Prometheus metrics endpoint
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
